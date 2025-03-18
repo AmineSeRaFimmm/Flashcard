8a13922a3fbed969ee8d4335630073291d7262f7
AmineSeRaFimmm <eduardowgx@gmail.com>
2025-03-18 16:18:13 +0800

    Update lesson1.js

--- chineseworkshop/lesson1.js
+++ chineseworkshop/lesson1.js
@@ -1,1 +1,7 @@
-
+let lesson1 = [
+    "朋友\t朋友\t...\tpéngyou\tFriend",
+    "家庭\t家庭\t...\tjiātíng\tFamily",
+    "工作\t工作\t...\tgōngzuò\tWork",
+    "旅行\t旅行\t...\tlǚxíng\tTravel",
+    "食物\t食物\t...\tshíwù\tFood"
+];
